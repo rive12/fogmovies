7b443289bfb50a3db9d57abdb3d8479b9171a259
RiveDev <128239680+rive12@users.noreply.github.com>
2024-02-28 17:32:34 -0500

    Add files via upload

---
+++ assets/TestView-CNLdKtpS.js
@@ -0,0 +1,2 @@
+import{r as o,j as e}from"./vendor-CZAh_B0X.js";import{B as i}from"./index-pUrgvmab.js";import"./auth-72H9TcpW.js";import"./caption-parsing-CCiqOzin.js";import"./react-dom-kkWylqtJ.js";import"./locales-CsHRJa3o.js";import"./Icons-CzXmvRCk.js";import"./language-db-DWRVP61y.js";import"./hls-Di-l_7QN.js";function x(){const[r,t]=o.useState(!1);if(r)throw new Error("I crashed");return e.jsx(i,{onClick:()=>t(!0),children:"Crash me!"})}export{x as default};
+//# sourceMappingURL=TestView-CNLdKtpS.js.map
